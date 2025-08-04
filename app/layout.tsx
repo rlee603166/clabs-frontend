e376224570e69b070a26228495733536cc387aa5
Ryan Lee <rlee603166@gmail.com>
2025-08-04 12:27:36 -0700

    tabs

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Circl',
+  description: 'Welcome to Circl!',
+  generator: 'Circl',
 }
 
 export default function RootLayout({
